48a4426db2d00386cd0b44e9a04ee745827568c5
Fabrice SALVAIRE <fabrice.salvaire@orange.fr>
2024-02-02 19:02:45 +0100

    update

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,10 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_LANGUAGE = 'en';
+export const SITE_TITLE = 'Fabrice Salvaire Home Page - fabrice-salvaire.fr';
+export const SITE_DESCRIPTION = 'Fabrice Salvaire Home Page';
+export const SITE_AUTHOR = 'Fabrice Salvaire';
+
+export const GITHUB_ACCOUNT = 'FabriceSalvaire';
+export const TWITTER_ACCOUNT = '@FabriceSalvaire';
