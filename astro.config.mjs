7d21f06d759c4745fca77732cfb82b3885cd14fb
Fabrice SALVAIRE <fabrice.salvaire@orange.fr>
2024-02-03 21:22:21 +0100

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,12 @@
 
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import react from "@astrojs/react";
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
-import react from "@astrojs/react";
+// https://github.com/natemoo-re/astro-icon
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
@@ -16,6 +18,15 @@ export default defineConfig({
 	mdx(),
 	sitemap(),
 	tailwind(),
-	react()
+	react(),
+	icon({
+	     include: {
+		 // mdi: ["*"], // (Default) Loads entire Material Design Icon set
+		 mdi: [
+		     'arrow-up-thin',
+		     'menu',
+		 ],
+	     },
+	}),
     ]
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,21 @@
 // -*- mode: typescript-ts -*-
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors')
+
 export default {
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
     theme: {
-	extend: {},
+	// https://uicolors.app
+	// https://www.w3schools.com/colors/colors_picker.asp
+	colors: {
+	    transparent: 'transparent',
+	    current: 'currentColor',
+	    black: colors.black,
+	    white: colors.white,
+	    gray: colors.gray,
+	    darkgray: '#35363C', // rgb(53, 54, 60) hsl(231, 6%, 22%)
+	},
     },
     plugins: [],
 }
